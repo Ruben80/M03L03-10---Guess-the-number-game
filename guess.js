bb64bb781b7366235f7f36adf072ad9fb4410f9c
Ruben80 <43403734+Ruben80@users.noreply.github.com>
2020-06-30 16:35:30 +0200

    Add files via upload

---
+++ guess.js
@@ -0,0 +1,4 @@
+naam = prompt("Welkom! Wat is je naam?")
+alert ("Hey " + naam + " ! Klik op OK om verder te gaan of druk op Enter");
+guess = prompt("Voer een nummer in van 0 tot 25 om te beginnen met raden...")
+
